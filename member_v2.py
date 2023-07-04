9352454fe7e43f948396f5ff366eaf6cbc173523
sudipmondal1310 <101926163+sudipmondal1310@users.noreply.github.com>
2023-07-04 16:21:37 +0530

    Update member_v2.py

--- member_v2.py
+++ member_v2.py
@@ -9,7 +9,8 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.vectorstores import FAISS 
-from speech_to_text import * #edited by Sudip
+#from speech_to_text import * #edited by Sudip
+from mic_access_streamlit import *
 import tempfile
 
 
